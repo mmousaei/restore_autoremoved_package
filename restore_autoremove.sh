b6ee8926c91b857ecc27fb94c9669502dae02aed
mmousaei <m.mousaei@gmail.com>
2018-02-28 05:37:26 -0500

    Create restore_autoremove.sh

---
+++ restore_autoremove.sh
@@ -0,0 +1,4 @@
+echo '#!/bin/bash' > restore
+echo sudo apt-get install `grep Remove /var/log/apt/history.log | tail -1 | sed -e 's|Remove: ||g' -e 's|([^)]*)||g' -e 's|:[^ ]* ||g' -e 's|,||g'` >> restore
+chmod +x restore 
+./restore
